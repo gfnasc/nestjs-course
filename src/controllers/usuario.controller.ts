500d713a820f93991bb2754afcf93aaba50afa10
Guilherme Nascimento <guilherme.nascimento@atlastechnol.com>
2023-04-16 19:49:26 -0300

    working with entities

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CriaUsuarioDTO } from 'src/dto/usuarios/criaUsuario.dto';
+import { UsuarioEntity } from 'src/entities/usuario.entity';
 import { UsuarioRepository } from 'src/repositories/usuario.repository';
+import { v4 as uuid } from 'uuid'
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -8,8 +10,15 @@ export class UsuarioController {
 
   @Post()
   async criaUsuario(@Body() data: CriaUsuarioDTO) {
-    this.usuarioRepository.salvar(data);
-    return data;
+    const usuario = new UsuarioEntity()
+    
+    usuario.id = uuid()
+    usuario.email = data.email
+    usuario.nome = data.nome
+    usuario.senha = data.senha
+
+    this.usuarioRepository.salvar(usuario);
+    return { message: 'created', id: usuario.id }
   }
   
   @Get()
